d01dc828a0117f675640b32fb0470133402b727e
eugenioLeal <eugenio.leal.333@gmail.com>
2018-01-09 18:27:51 -0600

    Added functions to remove duplicate code :zap:

--- Matrix-Multiplication/main.c
+++ Matrix-Multiplication/main.c
@@ -1,33 +1,17 @@
 /*
   Eugenio Leal A01022983
-  Matrix Multiplication
+  Matrix Multiplication:
+    Reads 2 Matrices from 2 separate files and multiplies them.
+  Requirements: C99 compatible compiler for variable sized arrays/matrix; for example: int matrix[m][n];
+  Compiler: gcc version 5.4.0
 */
 
 #include <stdio.h>
 #include <stdlib.h> // atoi, atof
 
-void print_matrix(int m, int n, float matrix[m][n]) {
-  for (size_t i = 0; i < m; i++) {
-    for (size_t j = 0; j < n; j++) {
-      printf("%.1f ", matrix[i][j]);
-    }
-    printf("\n");
-  }
-}
-
-void multiply_matrix(int m, int n, int p, float matrixA[m][n], float matrixB[n][p]) {
-  float sum;
-  float result[m][p];
-  for (size_t i = 0; i < m; i++) {
-    for (size_t j = 0; j < p; j++) {
-      sum = 0;
-      for (int k = 0; k < n; k++) {
-        sum = sum + *(*(matrixA + i) + k) * *(*(matrixB + k) + j);
-      }
-      *(*(result + i) + j) = sum;
-    }
-  }
-}
+// Function Declarations
+void print_matrix(int m, int n, float matrix[m][n]);
+void multiply_matrix(int m, int n, int p, float matrixA[m][n], float matrixB[n][p], float result[m][p]);
 
 int main(int argc, char const *argv[]) {
   // file i/o
@@ -48,26 +32,16 @@ int main(int argc, char const *argv[]) {
   n = atoi(buff);
   // Declare matrix
   float matrixA[m][n];
-
-  printf("Matrix A: \n");
+  // Filling Matrix A
   for (size_t i = 0; i < m; i++) {
     for (size_t j = 0; j < n; j++) {
       fscanf(fp, "%s", buff);
       matrixA[i][j] = atof(buff);
-      printf("%.1f ", matrixA[i][j]);
     }
-    printf("\n");
   }
-  printf("\n");
-  // testing function
+  // Print Matrix A
+  printf("Matrix A: \n");
   print_matrix(m, n, matrixA);
-  // testing without function
-  for (size_t i = 0; i < m; i++) {
-    for (size_t j = 0; j < n; j++) {
-      printf("%.1f ", *(*(matrixA + i) + j));
-    }
-    printf("\n");
-  }
 
   //-----------------------------------//
   //--- Matrix B: Reading from file ---//
@@ -85,42 +59,53 @@ int main(int argc, char const *argv[]) {
   }
   fscanf(fp, "%s", buff);
   p = atoi(buff);
-
-  float matrixB[100][100];
-
-  printf("Matrix B:\n");
+  // Declare Matrix B
+  float matrixB[n][p];
+  // Fill Matrix
   for (size_t i = 0; i < n; i++) {
     for (size_t j = 0; j < p; j++) {
       fscanf(fp, "%s", buff);
       matrixB[i][j] = atof(buff);
-      printf("%.1f ", matrixB[i][j]);
     }
-    printf("\n");
   }
-  printf("\n");
+  // Print Matrix B
+  printf("Matrix B:\n");
+  print_matrix(n, p, matrixB);
 
+  //-------------------------------------------------------//
+  //--- Matrix Multiplication using Iterative Algorithm ---//
+  //-------------------------------------------------------//
   float result[m][p];
-  // Start Matrix Multiplication
-  // Iterative algorithm
+  multiply_matrix(m, n, p, matrixA, matrixB, result);
+  // Print Result
+  printf("Result: \n");
+  print_matrix(m, p, result);
+
+  fclose(fp);
+  return 0;
+}
+
+//-------------------------------------------------------//
+//--- Matrix Multiplication using Iterative Algorithm ---//
+//-------------------------------------------------------//
+void multiply_matrix(int m, int n, int p, float matrixA[m][n], float matrixB[n][p], float result[m][p]) {
   float sum;
   for (size_t i = 0; i < m; i++) {
     for (size_t j = 0; j < p; j++) {
       sum = 0;
       for (int k = 0; k < n; k++) {
-        // testing: printf("%f + %f * %f\n", sum, matrixA[i][k], matrixB[k][j] );
-        sum = sum + matrixA[i][k] * matrixB[k][j];
+        sum = sum + *(*(matrixA + i) + k) * *(*(matrixB + k) + j);
       }
-      result[i][j] = sum;
+      *(*(result + i) + j) = sum;
     }
   }
-  printf("Result: \n");
-  for (size_t i = 0; i < 4; i++) {
-    for (size_t j = 0; j < 4; j++) {
-      printf("%.1f ", result[i][j]);
+}
+
+void print_matrix(int m, int n, float matrix[m][n]) {
+  for (size_t i = 0; i < m; i++) {
+    for (size_t j = 0; j < n; j++) {
+      printf("%.1f ", matrix[i][j]);
     }
     printf("\n");
   }
-
-  fclose(fp);
-  return 0;
 }
